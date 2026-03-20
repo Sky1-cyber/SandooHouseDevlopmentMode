abfd682fa4ef0a4474dfa846cc189e6e49d18fd9
Sky1-cyber <tongheng78987@gmail.com>
2026-03-20 16:50:20 +0700

    Dashboard and pos ready Upload

---
+++ Controllers/SaleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sandoohouse.Controllers;
+
+public class SaleController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return RedirectToAction("Index", "ComingSoon");
+    }
+}

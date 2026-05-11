80fece17d53b47164f304f5603765920673ce9dc
Sky1-cyber <tongheng78987@gmail.com>
2026-05-11 15:44:43 +0700

    Update Backend dashboard

--- Controllers/ExpensesController.cs
+++ Controllers/ExpensesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Sandoohouse.ApplicationProgram;
 
 namespace Sandoohouse.Controllers;
@@ -13,6 +14,7 @@ public class ExpensesController : Controller
         
     // GET
     [HttpGet]
+    [Authorize(Roles = "SuperAdmin,Owner,Manager")]
     public IActionResult Index()
     {
         return View();
